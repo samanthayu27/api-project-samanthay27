a3f00a143779ea54fbd101ba24789631241f3cd0
samanthayu27 <samanthay27@nycstudents.net>
2024-01-07 17:27:24 -0500

    change theme doesnt work :( ask whalen for help

--- api-project/js/domselectors.js
+++ api-project/js/domselectors.js
@@ -2,6 +2,9 @@ const DOMselectors = {
     changetheme: document.querySelector("#changetheme"),
     cardcontainer: document.querySelector('.cardcontainer'),
     card: document.querySelector('#card'),
-}
+    buttons: document.querySelectorAll(".vision"),
+    searchfor: document.querySelector("#searchfor"),
+    form: document.querySelector("#form"),
+};
 
-export {DOMselectors};
+export { DOMselectors };
